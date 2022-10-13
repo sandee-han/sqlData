2077dad721df16cc06aaba9f60eb555cd7f68809
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-13 13:16:00 +0900

    replaceAllQuot method 추가 및 parsing

--- src/main/java/com/line/domain/Hospital.java
+++ src/main/java/com/line/domain/Hospital.java
@@ -7,12 +7,36 @@ public class Hospital {
     private String category;
     private Integer emergencyRoom;  // snake camel
     private String name;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Integer getEmergencyRoom() {
+        return emergencyRoom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubdivision() {
+        return subdivision;
+    }
+
     private String subdivision;
 
-
-
-    public Hospital(String id) {
-        this.id = id.replaceAll("\"", "");
+    public Hospital(String id, String address) {
+        this.id = id;
+        this.address = address;
     }
 
     public String getId() {
