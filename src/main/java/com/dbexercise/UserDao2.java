8dde8aad91322055043cce1585d4da005b879b64
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-17 13:48:23 +0900

    Select

--- src/main/java/com/dbexercise/UserDao2.java
+++ src/main/java/com/dbexercise/UserDao2.java
@@ -1,14 +1,16 @@
 package com.dbexercise;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import com.dbexercise.domain.User;
+
+import java.sql.*;
 import java.util.Map;
 
 public class UserDao2 {
     public void add() throws SQLException, ClassNotFoundException {
         Map<String, String> env = System.getenv();
+
+        // 환경변수에서 가져옴
+        // 환경변수를 설정함으로 보안 유지
         String dbHost = env.get("DB_HOST");
         String dbUser = env.get("DB_USER");
         String dbPassword = env.get("DB_PASSWORD");
@@ -16,17 +18,44 @@ public class UserDao2 {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);  // db 연결
         PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES(?, ?, ?)");
-        ps.setString(1, "1");
-        ps.setString(2, "kangong");
-        ps.setString(3, "1123");
+        ps.setString(1, "3");
+        ps.setString(2, "ale");
+        ps.setString(3, "1234");
 
+        int status = ps.executeUpdate();    // ctrl + enter
+        //  executeUpdate 반환값 : row cnt
+
+        System.out.println(status);
         ps.executeUpdate(); // ctrl + enter
         ps.close();
         conn.close();
+        System.out.println("DB Insert 완료");
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);  // db 연결
+        PreparedStatement ps = conn.prepareStatement("SELECT id, name, password FROM users WHERE id = ?");
+        ps.setString(1, id);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+        rs.close();
+        ps.close();
+        conn.close();
+
+        return user;
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao2 userDao2 = new UserDao2();
-        userDao2.add();
+        User user = userDao2.get("1");
+        System.out.println(user.getName());
     }
 }
---
+++ src/main/java/com/dbexercise/domain/User.java
@@ -0,0 +1,30 @@
+package com.dbexercise.domain;
+
+// DO : data object
+// VO : value object
+public class User {
+    private String id;
+    private String name;
+
+    private String password;
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User() {
+    }
+}
